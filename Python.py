04b551bddbddb750c79fb775b00acc8af04066fb
kodikaraek.206059M <kodikaraek.20@uom.lk>
2023-10-14 23:51:36 +0530

    Python

---
+++ Python.py
@@ -0,0 +1,29 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_numbers = []
+odd_numbers = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        odd_numbers.append(number)
+
+print(f'Even numbers: {even_numbers}')
+print(f'Odd numbers: {odd_numbers}')
+
+
+# Creat a square
+
+import turtle
+
+# Create a turtle object
+t = turtle.Turtle()
+
+# Draw a square
+for _ in range(4):
+    t.forward(100)  # Move forward by 100 units
+    t.right(90)     # Turn right by 90 degrees
+
+# Close the turtle graphics window on click
+turtle.done()
+
